226dc2c32220b699333f3444e538ce7a91243389
Zubair UnderWood <finalfantasy.fantasy9@gmail.com>
2024-05-26 21:17:09 +0500

    learning

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ModelComponent } from './model/model.component';
-import { StoreComponent } from './store/store.component';
 import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ModelComponent,
-    StoreComponent,
     AdminComponent
   ],
 
