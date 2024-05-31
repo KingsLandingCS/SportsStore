bc409109d4094a255d2d0906866077889c049135
Zubair UnderWood <finalfantasy.fantasy9@gmail.com>
2024-05-31 08:13:52 +0500

    learning

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,25 @@ import { AppComponent } from './app.component';
 import { ModelComponent } from './model/model.component';
 import { AdminComponent } from './admin/admin.component';
 import { StoreModule } from "./store/store.module";
+import { RouterModule } from '@angular/router';
+import { StoreComponent } from './store/store.component';
+import { CheckoutComponent } from './store/checkout.component';
+import { CartDetailComponent } from './store/cartDetail.component';
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        ModelComponent,
-        AdminComponent
-    ],
-    providers: [],
-    bootstrap: [AppComponent],
-    imports: [
-        BrowserModule,
-        StoreModule
-    ]
+  declarations: [
+    AppComponent,
+    ModelComponent,
+    AdminComponent
+  ],
+  imports: [BrowserModule, StoreModule,
+    RouterModule.forRoot([
+      { path: "store", component: StoreComponent },
+      { path: "cart", component: CartDetailComponent },
+      { path: "checkout", component: CheckoutComponent },
+      { path: "**", redirectTo: "/store" }
+    ])],
+  providers: [],
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
